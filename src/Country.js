88f9126cf386760dac77bb442197673e38b541b1
David <mobb90@o2.pl>
2018-01-06 11:11:48 +0100

    Fixed shuffle and some small bugs

--- src/Country.js
+++ src/Country.js
@@ -8,10 +8,10 @@ class Country extends Component {
             countries: [],
             randomCountry: {},
             randomOptions: [],
-            userIsWin: '',
+            userIsWin: undefined,
             disableFieldset: false,
             goodGuess: 0,
-            bgColor: {backgroundColor: 'white'}
+            bgColor: {backgroundColor: '#FFFFFF'}
         }
         this.getRandomCountry = this.getRandomCountry.bind(this);
         this.checkWin = this.checkWin.bind(this);
@@ -32,43 +32,52 @@ class Country extends Component {
         const randomOpt2 = this.state.countries[Math.floor(Math.random()*this.state.countries.length)];
         const randomOpt3 = this.state.countries[Math.floor(Math.random()*this.state.countries.length)];
         const randomOptions = [random.name, randomOpt1.name, randomOpt2.name, randomOpt3.name];
-        randomOptions.sort(() => { return 0.5 - Math.random() });
+        
+        function shuffleArray(array) {
+            for (let i = array.length - 1; i > 0; i--) {
+                let j = Math.floor(Math.random() * (i + 1));
+                [array[i], array[j]] = [array[j], array[i]];
+            }
+        }
+
+        shuffleArray(randomOptions);
+
         this.setState({
             randomCountry: random,
             randomOptions: randomOptions,
-            userIsWin: '',
+            userIsWin: undefined,
             disableFieldset: false
         })
     }
 
-    checkWin(e) {
+    checkWin(event) {
         this.setState({
             disableFieldset: true
         })
         const winCountry = this.state.randomCountry.name;
-        const userGuess = e.target.value;
+        const userGuess = event;
+
         if (winCountry === userGuess) {
             this.setState({
-                userIsWin: 'Win',
+                userIsWin: true,
                 goodGuess: this.state.goodGuess + 1,
                 bgColor: {backgroundColor: '#81C784'}
             })
         } else {
             this.setState({
-                userIsWin: 'Lose',
+                userIsWin: false,
                 bgColor: {backgroundColor: '#FF8A65'}
             })
         }
+
         setTimeout(()=>{
             this.getRandomCountry();
             this.setState({
-                userIsWin: '',
+                userIsWin: undefined,
                 disableFieldset: false,
-                bgColor: {backgroundColor: 'white'}
+                bgColor: {backgroundColor: '#FFFFFF'}
             })
-            console.log(e.target)
         }, 2000)
-
     }
 
 
@@ -81,17 +90,35 @@ class Country extends Component {
                     <div className="img-container">
                         <img className="mui-panel" src={this.state.randomCountry.flag} alt="Country flag" />
                     </div>
-                    <h2>{this.state.userIsWin === 'Win' ? 'You guess right! ' : ''}
-                        {this.state.userIsWin === 'Lose' ? 'You guess wrong. ' : ''} 
-                        Score:{this.state.goodGuess}</h2>
+                    <h2>{this.state.userIsWin === true ? 'You guess right! ' : ''}
+                        {this.state.userIsWin === false ? 'You guess wrong. ' : ''} 
+                        Score: {this.state.goodGuess}</h2>
                 </div>
                 <fieldset disabled={this.state.disableFieldset}>
-                    <form onClick={e => this.checkWin(e)}>
-                        <button className="mui-btn mui-btn--raised" value={this.state.randomOptions[0]}>{this.state.randomOptions[0]}</button>
-                        <button className="mui-btn mui-btn--raised" value={this.state.randomOptions[1]}>{this.state.randomOptions[1]}</button>
-                        <button className="mui-btn mui-btn--raised" value={this.state.randomOptions[2]}>{this.state.randomOptions[2]}</button>
-                        <button className="mui-btn mui-btn--raised" value={this.state.randomOptions[3]}>{this.state.randomOptions[3]}</button>
-                    </form>
+                    <div className="btn-wrapper">
+                        <button className="mui-btn mui-btn--raised" 
+                            value={this.state.randomOptions[0]}
+                            onClick={() => this.checkWin(this.state.randomOptions[0])}>
+                            {this.state.randomOptions[0]}
+                        </button>
+                        <button className="mui-btn mui-btn--raised" 
+                            value={this.state.randomOptions[1]}
+                            onClick={() => this.checkWin(this.state.randomOptions[1])}>
+                            {this.state.randomOptions[1]}
+                        </button>
+                        <button 
+                            className="mui-btn mui-btn--raised" 
+                            value={this.state.randomOptions[2]}
+                            onClick={() => this.checkWin(this.state.randomOptions[2])}>
+                            {this.state.randomOptions[2]}
+                        </button>
+                        <button 
+                            className="mui-btn mui-btn--raised" 
+                            value={this.state.randomOptions[3]}
+                            onClick={() => this.checkWin(this.state.randomOptions[3])}>
+                            {this.state.randomOptions[3]}
+                        </button>
+                    </div>
                 </fieldset>
             </div>
         )
